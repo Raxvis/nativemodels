2b633bd135df4fe8d85ee67a041ae15d0a265091
William <william@prefinem.com>
2018-09-24 14:16:41 -0500

    fixes #9

--- src/customtypes/index.js
+++ src/customtypes/index.js
@@ -1,5 +1,7 @@
+const enumerable = require('./enumerable');
 const guid = require('./guid');
 
 module.exports = {
+	enumerable,
 	guid,
 };
---
+++ tests/customtypes/enumerable.test.js
@@ -0,0 +1,41 @@
+/* global test, expect */
+
+const {
+	createModel,
+	customtypes: { enumerable },
+} = require('./../source');
+
+test('customtype | guid - invalid', () => {
+	const model = createModel({ enumerable: enumerable(['FOO', 'BAR']) });
+
+	expect(() => {
+		model({ enumerable: '' });
+	}).toThrow();
+	expect(() => {
+		model({ enumerable: 1 });
+	}).toThrow();
+	expect(() => {
+		model({ enumerable: 'BAZ' });
+	}).toThrow();
+});
+
+test('customtype | enumerable - valid', () => {
+	const model = createModel({ enumerable: enumerable(['FOO', 'BAR']) });
+
+	expect(model({ enumerable: 'FOO' })).toEqual({ enumerable: 'FOO' });
+	expect(model({ enumerable: 'BAR' })).toEqual({ enumerable: 'BAR' });
+});
+
+test('customtype | enumerable - allow null', () => {
+	const model = createModel({ enumerable: enumerable(['FOO', 'BAR']).nullable() });
+
+	expect(model({ enumerable: null })).toEqual({ enumerable: null });
+});
+
+test('customtype | enumerable - make sure required works', () => {
+	const model = createModel({ enumerable: enumerable(['FOO', 'BAR']).required() });
+
+	expect(() => {
+		model();
+	}).toThrow();
+});
