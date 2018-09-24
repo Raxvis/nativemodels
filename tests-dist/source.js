ca6ffc4a4cd3662420d682d7ae6fd265e9c97dc7
William <william@prefinem.com>
2018-09-24 13:02:44 -0500

    fixing source

--- tests-dist/source.js
+++ tests-dist/source.js
@@ -1,3 +1,3 @@
-const nativemodels = __dirname.indexOf('test-dist') > -1 ? require('./../dist') : require('./../src');
+const nativemodels = __dirname.indexOf('tests-dist') > -1 ? require('./../dist') : require('./../src');
 
 module.exports = nativemodels;
