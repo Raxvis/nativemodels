fe9152cf0502d69e2e82cbab4685a6fbfd17b4d5
William <william@prefinem.com>
2018-09-24 13:37:53 -0500

    remove dist and dist tests

--- tests-dist/datatypes/object.test.js
+++
@@ -1,8 +0,0 @@
-/* global test, expect */
-
-const { createModel } = require('./../source');
-const { userData, userResult, userSchema } = require('./../setup');
-
-test('datatype | object - basic object test', () => {
-	expect(createModel(userSchema)(userData)).toEqual(userResult);
-});
--- tests-dist/source.js
+++
@@ -1,3 +0,0 @@
-const nativemodels = __dirname.indexOf('tests-dist') > -1 ? require('./../dist') : require('./../src');
-
-module.exports = nativemodels;
