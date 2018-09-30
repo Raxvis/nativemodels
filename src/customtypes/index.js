e7f369be63e6d14b0e890ebd0ff79f52f00a06f9
William <william@prefinem.com>
2018-09-30 15:01:39 -0500

    fixes #12

--- src/customtypes/index.js
+++ src/customtypes/index.js
@@ -1,7 +1,9 @@
 const enumerable = require('./enumerable');
 const guid = require('./guid');
+const url = require('./url');
 
 module.exports = {
 	enumerable,
 	guid,
+	url,
 };
---
+++ src/customtypes/url.js
@@ -0,0 +1,14 @@
+const base = require('./../datatypes/base');
+
+const url = () => ({
+	...base,
+	validate(value, name) {
+		if (/^(https?|ftp):\/\/(-\.)?([^\s/?.#-]+\.?)+(\/[^\s]*)?$/iu.test(value)) {
+			return true;
+		}
+
+		throw new Error(`Property ${name} (${value}) is not a URL`);
+	},
+});
+
+module.exports = url;
