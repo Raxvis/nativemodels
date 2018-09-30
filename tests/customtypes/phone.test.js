3afed383812e9d3cb5532347830242703de3a3d5
William <william@prefinem.com>
2018-09-30 15:08:36 -0500

    adding more test cases

--- tests/customtypes/phone.test.js
+++ tests/customtypes/phone.test.js
@@ -6,7 +6,7 @@ const {
 } = require('./../source');
 
 const validPhoneNumbers = ['123-456-7890', '(123) 456-7890', '123 456 7890', '123.456.7890', '+91 (123) 456-7890'];
-const invalidPhoneNumbers = [''];
+const invalidPhoneNumbers = ['', 'a', '123412345456', 1234567890];
 
 test('customtype | phone - invalid', () => {
 	const model = createModel({ phone: phone() });
