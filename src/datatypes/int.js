2cc3fca875ead6b6588b0775a50c36942161bef5
William <william@prefinem.com>
2018-11-28 10:25:50 -0600

    pull non strict checking into it's own function

--- src/datatypes/int.js
+++ src/datatypes/int.js
@@ -1,19 +1,15 @@
 const base = require('./base');
 
+const nonStrictCheck = (value) =>
+	typeof value !== 'boolean' && value !== '' && !isNaN(parseInt(value)) && parseInt(value) === parseFloat(value);
+
 const isValidInt = (key, value, strict = false) => {
 	if (typeof value === 'number' && parseInt(value) === parseFloat(value)) {
 		return true;
 	}
 
-	if (!strict) {
-		if (
-			typeof value !== 'boolean' &&
-			value !== '' &&
-			!isNaN(parseInt(value)) &&
-			parseInt(value) === parseFloat(value)
-		) {
-			return true;
-		}
+	if (!strict && nonStrictCheck(value)) {
+		return true;
 	}
 
 	throw new Error(`Property ${key} is not an int`);
