911c97985221da727cb34f28e2dd02d07f065e36
William <william@prefinem.com>
2019-01-24 10:48:40 -0600

    circlci config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
 	collectCoverageFrom: ['src/**.{js,jsx}', '!**/node_modules/**'],
 	coverageDirectory: './coverage/',
 	coverageReporters: ['lcov'],
+	reporters: ['default', 'jest-junit'],
 	testPathIgnorePatterns: ['/node_modules/', '/site/'],
 };
