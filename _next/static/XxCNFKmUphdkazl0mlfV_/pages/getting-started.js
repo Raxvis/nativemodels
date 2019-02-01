bf195f37b6250991fa2db355d2a2461243b196ec
William <william@prefinem.com>
2019-02-01 10:20:26 -0600

    add site to git

---
+++ _next/static/XxCNFKmUphdkazl0mlfV_/pages/getting-started.js
@@ -0,0 +1,3 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{625(e,n,t){__NEXT_REGISTER_PAGE("/getting-started",() => e.exports=t(626),{page:e.exports.default})},626(e,n,t){t.r(n);const a=t(54),o=t(0),r=t.n(o);
+
+n.default=function(){return r.a.createElement(a.a,null,r.a.createElement("div",{className:"p-16"},"Coming Soon"))}}},[[625,1,0]]]);
---
+++ _next/static/webpack/static/development/pages/getting-started.js.07ec83548cb0237eff6f.hot-update.js
@@ -0,0 +1,96 @@
+webpackHotUpdate("static/development/pages/getting-started.js",{
+
+/** */ "./components/Layout.js":
+/* !******************************!*\
+  !*** ./components/Layout.js ***!
+  \******************************/
+/* ! exports provided: default */
+/** */ (function(module, __webpack_exports__, __webpack_require__) {
+
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ const next_head__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/* ! next/head */ "./node_modules/next/head.js");
+/* harmony import */ const next_head__WEBPACK_IMPORTED_MODULE_0___default = /* #__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ const _Nav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/* ! ./Nav */ "./components/Nav.js");
+/* harmony import */ const react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/* ! react */ "./node_modules/react/index.js");
+/* harmony import */ const react__WEBPACK_IMPORTED_MODULE_2___default = /* #__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+const _jsxFileName = "/Users/william/Sync/nativemodels/site/components/Layout.js";
+
+
+
+
+const Layout = function Layout(_ref) {
+  const children = _ref.children;
+
+  return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", {
+    className: "font-sans antialiased",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(next_head__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("meta", {
+    charset: "utf-8",
+    key: "charset",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("meta", {
+    content: "width=device-width, initial-scale=1, shrink-to-fit=no",
+    key: "viewport",
+    name: "viewport",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("title", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, "Nativemodel Docs"), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("link", {
+    href: "https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css",
+    rel: "stylesheet",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("style", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "html, body{margin: 0; padding: 0}")), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(_Nav__WEBPACK_IMPORTED_MODULE_1__.default, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", {
+    className: "mt-16",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, children));
+};
+
+/* harmony default export */ __webpack_exports__.default = (Layout);
+
+/** */ })
+
+})
+// # sourceMappingURL=getting-started.js.07ec83548cb0237eff6f.hot-update.js.map
