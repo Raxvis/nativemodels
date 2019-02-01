46e94c33e229f0d64a4012dd65629dad7aa4efbb
William <william@prefinem.com>
2019-02-01 10:34:14 -0600

    reducing complexity of string datatype

--- src/datatypes/string.js
+++ src/datatypes/string.js
@@ -1,6 +1,25 @@
 const createType = require('./../createType');
 const { isString } = require('./../lib/checks/types');
 
+const strictCheck = (key, value, options) => {
+	if (isString(value)) {
+		if (options.length && value.length > options.length) {
+			throw new Error(`NativeModels - Property ${key} is longer than ${options.length}`);
+		}
+		return true;
+	}
+
+	throw new Error(`NativeModels - Property ${key} is not a string`);
+};
+
+const validCheck = (key, value) => {
+	if (value === null) {
+		throw new Error(`NativeModels - Property ${key} is not a string`);
+	}
+
+	return true;
+};
+
 const string = (options = {}) =>
 	createType({
 		name: 'string',
@@ -11,23 +30,8 @@ const string = (options = {}) =>
 
 			return `${value}`;
 		},
-		strictCheck: (key, value) => {
-			if (isString(value)) {
-				if (options.length && value.length > options.length) {
-					throw new Error(`NativeModels - Property ${key} is longer than ${options.length}`);
-				}
-				return true;
-			}
-
-			throw new Error(`NativeModels - Property ${key} is not a string`);
-		},
-		validCheck: (key, value) => {
-			if (value === null) {
-				throw new Error(`NativeModels - Property ${key} is not a string`);
-			}
-
-			return true;
-		},
+		strictCheck: (key, value) => strictCheck(key, value, options),
+		validCheck,
 	});
 
 module.exports = string;
