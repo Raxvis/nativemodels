a9e6759ce9b4768bf3a284ba033090fa77ff3f09
William <william@prefinem.com>
2019-02-01 10:22:55 -0600

    Deploy to gh-pages

---
+++ site/out/_next/static/XfbO_8d3vJ0Pa55tyiu4x/pages/getting-started.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{247:function(e,n,t){__NEXT_REGISTER_PAGE("/getting-started",function(){return e.exports=t(248),{page:e.exports.default}})},248:function(e,n,t){"use strict";t.r(n);var a=t(17),o=t(0),r=t.n(o);n.default=function(){return r.a.createElement(a.a,null,r.a.createElement("div",{className:"p-16"},"Coming Soon"))}}},[[247,1,0]]]);
