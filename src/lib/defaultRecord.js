9fa41e46aadc8f5b0794d687536d6cfd98ad7a84
William <william@prefinem.com>
2019-04-26 14:07:50 -0500

    allow default value to be a function and executed

--- src/lib/defaultRecord.js
+++ src/lib/defaultRecord.js
@@ -1,9 +1,11 @@
+const defaultValue = require('./defaultValue');
+
 const defaultRecord = (schema, record) => ({
 	...record,
 	...Object.keys(schema).reduce(
 		(result, key) => ({
 			...result,
-			...(schema[key].hasDefault ? { [key]: record[key] || schema[key].defaultValue } : {}),
+			...(schema[key].hasDefault ? { [key]: record[key] || defaultValue(schema[key].defaultValue) } : {}),
 		}),
 		{},
 	),
