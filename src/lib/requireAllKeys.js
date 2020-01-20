9eaf49dc4c70226cadd2322a39737525854a9825
sfeldkamp <sfeldkamp@distributorcentral.com>
2020-01-20 09:24:23 -0600

    list all extra keys in error message

--- src/lib/requireAllKeys.js
+++ src/lib/requireAllKeys.js
@@ -1,8 +1,9 @@
-const strictCheck = (schema, record) =>
-	Object.keys(record).forEach((key) => {
-		if (!schema[key]) {
-			throw new Error(`NativeModels - Property: '${key}' is not defined in the schema`);
-		}
-	});
+const strictCheck = (schema, record) => {
+	const extraKeys = Object.keys(record).filter((key) => !schema[key]);
+
+	if (extraKeys.length) {
+		throw new Error(`NativeModels - Properties: '${extraKeys.join()}' not defined in the schema`);
+	}
+};
 
 module.exports = strictCheck;
