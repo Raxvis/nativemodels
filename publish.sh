3788ac1c712155be15a54809c8db41c15de83502
William <william@prefinem.com>
2020-01-21 16:57:16 -0600

    version bump to fix version issue

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ rm -rf ./dist/*
 cp -r ./src/ ./dist
 cp ./package.json ./dist/package.json
 cp ./README.md ./dist/README.md
-npm publish dist
+npm publish dist/
 # npm publish --tag canary
 rm -rf ./dist
