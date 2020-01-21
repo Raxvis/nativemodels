b00d451aad175e90c84db6865bf5b566bb38240b
William <william@prefinem.com>
2020-01-21 17:20:22 -0600

    only publish source files

--- publish.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p dist/
-rm -rf ./dist/*
-cp -r ./src/ ./dist
-cp ./package.json ./dist/package.json
-cp ./README.md ./dist/README.md
-npm publish dist/
-# npm publish --tag canary
-rm -rf ./dist
